3859b01c2b455f8b0492e2c6cedd7fab270f262e
Santiago Rojas <sajoraho@gmail.com>
2021-10-08 21:36:04 -0500

    Cambio FK Loan de User a Register

--- authApp/serializers/loanSerializer.py
+++ authApp/serializers/loanSerializer.py
@@ -3,4 +3,4 @@ from rest_framework import serializers
 class LoanSerializer(serializers.ModelSerializer):
     class Meta:
         model = Loan
-        fields = ['id', 'date_borrow', 'date_agreed', 'date_return', 'book', 'user']
+        fields = ['id', 'date_borrow', 'date_agreed', 'date_return', 'book', 'register']
