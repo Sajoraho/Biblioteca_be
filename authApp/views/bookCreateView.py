4fda70f52136abaff95768e62a64fc04885bbf1a
Santiago Rojas <sajoraho@gmail.com>
2021-10-28 23:26:27 -0500

    Cambios en el view de Book

--- authApp/views/bookCreateView.py
+++ authApp/views/bookCreateView.py
@@ -1,6 +1,5 @@
 from rest_framework import status, generics 
 from rest_framework.response import Response
-from rest_framework.serializers import Serializer 
 from authApp.serializers.bookSerializer import BookSerializer
 from authApp.models.book import Book
 from rest_framework.decorators import action
@@ -12,8 +11,8 @@ class BookCreateView(generics.ListCreateAPIView):
     @action(methods=['post'], detail=True)
     def post(self, request, *args, **kwargs):
         serializer = BookSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save
+        serializer.is_valid(data=request.data)
+        serializer.save()
 
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     
